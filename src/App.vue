81d938b1194cb748c2864beba767595dbc5b9a19
Sergio Dobri <sergiodobri@gmail.com>
2021-06-23 18:19:30 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="relative min-h-screen text-gray-900 font-sans antialiased leading-normal tracking-normal font-normal bg-white"
+  >
+    <router-view />
+  </div>
+</template>
