5c5fbda79fcbe93c64433ae876bbe23c1e961048
Sergio Dobri <sergiodobri@gmail.com>
2021-07-05 20:53:05 -0300

    add local fonts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-white">
-    <div class="max-w-7xl bg-local bg mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="max-w-7xl bg-local bg mx-auto px-4 sm:px-6 lg:px-8">
       <h1>Home</h1>
     </div>
   </div>
