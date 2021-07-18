a46c9559643b8c2fc99a2323525b7728eb3b6cbe
Sergio Dobri <sergiodobri@gmail.com>
2021-07-18 07:56:39 -0300

    fix init position

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen bg-white">
+  <div class="h-screen bg-gray-100 pt-96">
     <div id="blog" class="layout">
       <h1>Blog</h1>
     </div>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen bg-white">
+  <div class="h-screen bg-white pt-96">
     <div class="layout">
       <h1>Início</h1>
     </div>
