39e3e2cf24d7625df1c5f92f59d60a8b1c35d3e0
Sergio Dobri <sergiodobri@gmail.com>
2021-07-21 23:59:37 -0300

    remove wrong commentted line

--- src/components/MenuToggler.vue
+++ src/components/MenuToggler.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    /* class="inline-flex items-center justify-center py-1.5 px-2 rounded-md border-2 text-gray-500 border-gray-500 hover:text-white hover:bg-orange-600" */
+    class="inline-flex items-center justify-center py-1.5 px-2 rounded-md border-2 text-gray-500 border-gray-500 hover:text-white hover:bg-orange-600"
     aria-controls="mobile-menu"
   >
     <Icon :name="iconName" class="h-6 w-6" />
