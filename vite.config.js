aeec2d2e78a5f7fb2d14e01df471a368c15d5f9d
Sergio Dobri <sergiodobri@gmail.com>
2021-07-22 10:08:25 -0300

    add vite-eslint

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 import ViteComponents from 'vite-plugin-components'
 import viteSvgIcons from 'vite-plugin-svg-icons'
 import path from 'path'
@@ -14,6 +15,7 @@ export default defineConfig({
   // hmr: { overlay: false },
   plugins: [
     vue(),
+    eslint(),
     ViteComponents(),
     viteSvgIcons({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
